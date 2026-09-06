e6ca1aa595cc5d5dc512057211c1664559219e3f
notmescooby-ui <not.mescooby@gmail.com>
2026-09-06 19:22:51 +0530

    playstore 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 android {
     namespace = "com.bluefarm.app"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
