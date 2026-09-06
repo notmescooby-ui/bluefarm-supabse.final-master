e597f1724dc09a8cf6c08faa62506c9fc3318cc9
notmescooby-ui <not.mescooby@gmail.com>
2026-09-06 18:12:39 +0530

    playstore 1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,11 +12,10 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.bluefarm"
+    namespace = "com.bluefarm.app"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bluefarm"
+        applicationId = "com.bluefarm.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -65,11 +64,4 @@ flutter {
 }
 
 dependencies {
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
-    implementation("com.google.firebase:firebase-analytics")
-
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth")
 }
